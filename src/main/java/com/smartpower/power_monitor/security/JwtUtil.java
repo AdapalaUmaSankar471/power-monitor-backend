c54c0d41d3e9cd2933f2a71b3431e9f24149ddb7
AdapalaUmaSankar471 <venkatanagavenuavula@gmail.com>
2026-03-18 20:38:58 +0530

    Fix: JWT secret from env variable

--- src/main/java/com/smartpower/power_monitor/security/JwtUtil.java
+++ src/main/java/com/smartpower/power_monitor/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.smartpower.power_monitor.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -10,7 +11,8 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-    private final String SECRET = "smartpowersecretkeysmartpowersecretkey";
+    @Value("${jwt.secret}")
+    private String SECRET;
 
     private Key getKey() {
         return Keys.hmacShaKeyFor(SECRET.getBytes());
